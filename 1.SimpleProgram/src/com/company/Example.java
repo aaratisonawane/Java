aaa5057a099a9a116417aba1ea5060b41cbfaccd
Aarati <aaratisonawane08@gmail.com>
2017-11-08 19:29:51 -0600

    first java program

---
+++ 1.SimpleProgram/src/com/company/Example.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        System.out.print("Ladies first ");// next msg will be on the same line because we have used only print
+        System.out.println("What's up Guys!");
+
+    }
+}
